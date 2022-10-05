3594248a19534299f597d956d76b851e08fa4c83
shokhrukh <mr.shohruh.1003@gmail.com>
2022-10-06 02:25:04 +0900

    finished setup

---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
